a47c0b631a6736a344c170aa56bc3b760c1c4f95
Ahmad Mahmoud <96989670+ja03@users.noreply.github.com>
2023-04-30 02:36:09 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const button = document.querySelectorAll("button")
+const input = document.querySelector("input")
+const output = document.querySelector("#output")
+const p = document.querySelector("#description")
+const img = document.querySelector("img")
+listItems = ["Waffel", "Pizza", "Burger", "Donuts", "Bakery", "Watermelon"]
+const descriptions = {
+    "Waffel": "Waffle - A pancake's crispy cousin that always brings a smile to your face with its delicious little square pockets just waiting to be filled with syrup and butter.",
+    "Pizza": "Pizza - A magical circle of love and cheese that brings people together, unless you're lactose intolerant, in which case it brings you closer to a toilet.",
+    "Burger": "Burger - A delicious stack of hope that promises happiness with every bite, until you realize you're now too full to do anything other than take a nap.",
+    "Donuts": "Donuts - A sugary ring of happiness that can make even the grumpiest person smile, unless of course, they're the type who thinks sprinkles are a crime against desserts.",
+    "Bakery": "Bakery - The magical land where flour and sugar become works of art that make you want to forget your diet and dive in face first.",
+    "Watermelon": "Watermelon - The fruit that promises a refreshing treat on a hot summer day, until you start spitting out all the seeds and look like a machine gunner at a fruit stand."
+};
+const descriptionImg = {
+    "Waffel": "./waffel.png",
+    "Pizza": "./pizza.png",
+    "Burger": "./burger.png",
+    "Donuts": "./donuts.png",
+    "Bakery": "./bakery.png",
+    "Watermelon": "./watermelon.png"
+};
+function toTitleCase(str) {
+    return str.toLowerCase().replace(/(?:^|\s|-)\w/g, function (match) {
+        return match.toUpperCase();
+    });
+}
+function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+function buttonAnimation() {
+    button[0].style.boxShadow = '0 .1cm 1px 0px red';
+    button[0].style.transform = 'translateY(-.03cm) skew(1deg)';
+    setTimeout(function () {
+        button[0].style.boxShadow = '0 .19cm 1px 0px red';
+        button[0].style.transform = 'translateY(-.09cm) skew(1deg)';
+    }, 250);
+}
+function buttonAnimation1() {
+    button[1].style.boxShadow = '0 .1cm 1px 0px white';
+    button[1].style.transform = 'translateY(-.03cm) skew(1deg)';
+    setTimeout(function () {
+        button[1].style.boxShadow = '0 .19cm 1px 0px white';
+        button[1].style.transform = 'translateY(-.09cm) skew(1deg)';
+    }, 250);
+}
+button[1].style.opacity = 0
+function Generate() {
+    inputValue = input.value
+    inputValue = toTitleCase(inputValue)
+    element = getRandomElement(listItems)
+    element = getRandomElement(listItems);
+    output.innerHTML = `your element is ${inputValue}'s ${element}`
+    p.innerHTML = `${descriptions[element]}`
+    img.src = `${descriptionImg[element]}`
+    button[1].style.opacity = 1
+}
+button[0].addEventListener("click", Generate)
+button[0].addEventListener("click", buttonAnimation);
+button[1].addEventListener("click", Generate)
+button[1].addEventListener("click", buttonAnimation1);
